f0df38e4a8a54740a924be1f9a06139343d341d4
Tommy Kananen <tkananen@udel.edu>
2015-03-15 14:59:09 -0400

    first commiuut

---
+++ src/q3/MyClassTestFile.java
@@ -0,0 +1,66 @@
+package q3;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MyClassTestFile {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public  void utReturnTrueTest() {
+		MyClass c = new MyClass();
+		
+		
+		
+		assertTrue("If this message shows, return true does not return true", c.ReturnTrue());
+		
+		
+		assertFalse("If this message shows, ReturnTrue does not equal False", c.ReturnTrue());
+		
+		
+		
+	}
+	@Test
+	public   void utReturnAddTwoNumbers(){
+		MyClass d = new MyClass();
+		
+		int correctSum = 5;
+		int num1 = 2;
+		int num2 = 3;
+		MyClass.AddTwoNumbers(num1, num2);
+		int falseSum = 6;
+		int falsenum1 = 4;
+		int falsenum2 = 5;
+		
+		assertTrue("If this message shows, num1 + num2 != correctSum", correctSum == MyClass.AddTwoNumbers(num1,  num2));
+		
+		assertTrue("If this message shows, falsenum1 + falsenum2 == falseSum", falseSum == d.AddTwoNumbers(falsenum1,  falsenum2)) ;
+		
+		
+		
+		
+		
+	}
+	
+
+}
